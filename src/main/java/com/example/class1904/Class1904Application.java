6fa834708b19322db99902edea7be422f865993c
17721 <1772189496@qq.com>
2021-09-06 20:45:54 +0800

    初始

---
+++ src/main/java/com/example/class1904/Class1904Application.java
@@ -0,0 +1,13 @@
+package com.example.class1904;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class1904Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Class1904Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/class1904/Class1904ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.class1904;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class1904ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
